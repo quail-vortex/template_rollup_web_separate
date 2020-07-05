ddf8f646cfee80f21f4815f5389f4d3ef283c101
Chihiro Kaimori <kaimori@uzuworks.net>
2020-07-05 23:14:22 +0900

    [add] basic files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # template_rollup_web_separate
-template_rollup_web_separate
+
+Mostly for personal use template.
+
+- rollup
+- js : babel (separated)
+- css : scss + Autoprefixer (separated)
+- IE11 supported
+- output just both develop & product
---
+++ rollup.css.config.js
@@ -0,0 +1,48 @@
+import resolve from '@rollup/plugin-node-resolve';
+import postcss from 'rollup-plugin-postcss';
+
+const cssfiles = {
+  style: './src/css_src/main.scss'
+}
+
+export default [{
+  input: cssfiles,
+  output: [
+    {
+      name: 'style',
+      dir: `${__dirname}/dist/assets`,
+      entryFileNames: 'css/[name].css',
+    }
+  ],
+  plugins: [
+    resolve(),
+    postcss({
+      use: 'sass',
+      extensions: ['.scss'],
+      minimize: false,
+      sourceMap: true,
+      config: { path: 'postcss.config.js' },
+      extract: 'css/style.css',
+    })
+  ],
+}, {
+  input: cssfiles,
+  output: [
+    {
+      name: 'style',
+      dir: `${__dirname}/dist/assets`,
+      entryFileNames: 'css/[name].min.css',
+    }
+  ],
+  plugins: [
+    resolve(),
+    postcss({
+      use: 'sass',
+      extensions: ['.scss'],
+      minimize: true,
+      sourceMap: false,
+      config: { path: 'postcss.config.js' },
+      extract: 'css/style.min.css',
+    })
+  ],
+}];
